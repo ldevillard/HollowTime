4b6625753522ba45d3bf6d7fc68e74e99eabf4d3
logandev <logandvllrd@gmail.com>
2025-02-19 21:11:31 +0100

    added cube visualization + minor clean

---
+++ Components/CubeVisualizer.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace HollowTime.Components
+{
+    public partial class CubeVisualizer : ComponentBase
+    {
+        [Parameter]
+        public string CurrentScramble { get; set; } = string.Empty;
+
+        bool is3D
+        {
+            get => _is3D;
+            set
+            {
+                _is3D = value;
+                buttoncLabel = value ? "2D" : "3D";
+            }
+        }
+        bool _is3D;
+        
+        string buttoncLabel = "3D";
+
+        void onButtonClicked()
+        {
+            is3D = !is3D;
+        }
+    }
+}
