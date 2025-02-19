6f7216e277aadf3ea921436a5e3960733210c305
Logan Devillard <71450527+ldevillard@users.noreply.github.com>
2025-02-19 16:37:51 +0100

    added scramble api call and display it on top of the timer

--- Pages/Home.razor.cs
+++ Pages/Home.razor.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
 
 namespace HollowTime.Pages
 {
@@ -8,10 +9,13 @@ namespace HollowTime.Pages
         HollowTime.Components.Timer? timer;
         HollowTime.Components.TimeStats? currentStats;
 
+        string currentScramble = String.Empty;
+
         protected override void OnAfterRender(bool firstRender)
         {
             if (firstRender && timer is not null)
             {
+                refreshScramble();
                 timer.OnTimerEnded += onTimerEnded;
             }
         }
@@ -30,7 +34,14 @@ namespace HollowTime.Pages
 
         void onTimerEnded(TimeSpan elapsedTime)
         {
+            refreshScramble();
             currentStats?.RecordTime(elapsedTime);
+        }
+
+        async void refreshScramble() 
+        {
+            currentScramble = await JS.InvokeAsync<string>("scrambleGenerator.getDefaultScramble");
+            StateHasChanged();
         }
 
         public void Dispose()
