e6da1b54ea998d881afdd22ee362cfcc02cfa774
Logan Devillard <71450527+ldevillard@users.noreply.github.com>
2025-02-20 12:19:56 +0100

    added npm packages

---
+++ NpmJS/src/index.js
@@ -0,0 +1,11 @@
+﻿const { Scrambow } = require('scrambow');
+
+window.scrambleGenerator =
+{
+    getDefaultScramble: () =>
+    {
+        const scrambler = new Scrambow();
+        scrambler.setType('333');
+        return scrambler.get()[0].scramble_string;
+    }
+};
